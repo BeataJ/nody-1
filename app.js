0337b161acde4466ef52eaf0be759b1817b47880
beataj <beata@jasinski.com>
2023-05-09 14:34:31 -0700

    create node server

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const userName = 'Beata';
-console.log(userName);
+const http = require('http');
+
+function handleRequest(req,res) {
+    res.statusCode = 200;
+    res.end('<h1>Hello from me!</h1>')
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
