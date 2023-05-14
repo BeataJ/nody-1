83d6cfb4359b3e74c47d0c20f631bdb0dc1fd510
beataj <beata@jasinski.com>
2023-05-14 16:50:50 -0700

    save data fs

--- app.js
+++ app.js
@@ -19,8 +19,14 @@ app.get('/', function(req, res) {
 app.post('/store-user', function(req,res){
     const userName = req.body.username;
 
-    const filePath = path.join(__dirname, 'data', 'users.json')
-    fs.writeFileSync(filePath);
+    const filePath = path.join(__dirname, 'data', 'users.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    existingUsers.push(userName);
+
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
     
     res.send('<h1>Username stored!</h1>')
