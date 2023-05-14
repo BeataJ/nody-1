2ff6e8ddaccbc6109d6bd4f3717fc434cd3c3835
beataj <beata@jasinski.com>
2023-05-13 23:25:28 -0700

    express get

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const http = require('http');
+const  express = require('express');
 
-function handleRequest(req,res) {
-    if(req.url === '/currenttime') {
-        res.statusCode = 200;
-        res.end("<h1>" + new Date().toISOString() + "</h1>");
-    } else if(req.url === '/') {
-        res.statusCode = 200;
-        res.end("<h1>Hello from me!</h1>");
-    }
-}
 
-const server = http.createServer(handleRequest);
+const app = express();
 
-server.listen(3000);
+
+app.get('/currenttime', function(req, res) {
+    res.send("<h1>" + new Date().toISOString() + "</h1>");
+})
+
+app.get('/', function(req, res) {
+    res.send("<h1>Hello from me!</h1>");
+})
+
+app.listen(3000)
+
+// const http = request('http');
+
+// function handleRequest(req,res) {
+//     if(req.url === '/currenttime') {
+//         res.statusCode = 200;
+//         res.end("<h1>" + new Date().toISOString() + "</h1>");
+//     } else if(req.url === '/') {
+//         res.statusCode = 200;
+//         res.end("<h1>Hello from me!</h1>");
+//     }
+// }
+
+// const server = http.createServer(handleRequest);
+
+// server.listen(3000);
